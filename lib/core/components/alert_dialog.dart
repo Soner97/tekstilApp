3f7ab4e3303c8107e987dd63178728474de5c9ef
Soner97 <sonerkaraevli@gmail.com>
2020-10-21 15:54:08 +0300

    some changes.

--- lib/core/components/alert_dialog.dart
+++ lib/core/components/alert_dialog.dart
@@ -1,56 +1,56 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-import 'package:tekstilcim/core/components/colors.dart';
-import 'package:tekstilcim/core/components/radiobutton.dart';
-import 'package:tekstilcim/core/components/text.dart';
-import 'package:tekstilcim/core/components/context_extension.dart';
+// import 'package:tekstilcim/core/components/colors.dart';
+// import 'package:tekstilcim/core/components/radiobutton.dart';
+// import 'package:tekstilcim/core/components/text.dart';
+// import 'package:tekstilcim/core/components/context_extension.dart';
 
-//Text('${nameRadio[index]}',
-// Navigator.of(context).pop();
-// ignore: must_be_immutable
-class AlertDialogWidget extends StatelessWidget {
-  final List<String> selectedValue;
-  final List<String> radioButtonName;
+// //Text('${nameRadio[index]}',
+// // Navigator.of(context).pop();
+// // ignore: must_be_immutable
+// class AlertDialogWidget extends StatelessWidget {
+//   final List<String> selectedValue;
+//   final List<String> radioButtonName;
 
-  const AlertDialogWidget({Key key, this.radioButtonName, this.selectedValue});
+//   const AlertDialogWidget({Key key, this.radioButtonName, this.selectedValue});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: context.dynamicHeight(0.06),
-      width: context.dynamicWidth(0.6),
-      padding: context.paddingLow,
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colorsx.mainColor,
-        ),
-        borderRadius: BorderRadius.all(Radius.circular(25)),
-      ),
-      child: FlatButton(
-          onPressed: () {
-            showGeneralDialog(
-                context: context,
-                barrierDismissible: true,
-                barrierLabel:
-                    MaterialLocalizations.of(context).modalBarrierDismissLabel,
-                barrierColor: Colors.black.withOpacity(0.8),
-                transitionDuration: const Duration(milliseconds: 300),
-                pageBuilder: (BuildContext buildContext, Animation animation,
-                    Animation secondaryAnimation) {
-                  return Center(
-                    child: Container(
-                        width: MediaQuery.of(context).size.width - 100,
-                        height: MediaQuery.of(context).size.height - 100,
-                        padding: EdgeInsets.all(20),
-                        color: Colors.white,
-                        child: RadioButtonWidget(
-                          radioButtonName: this.radioButtonName,
-                          selectedValue: selectedValue,
-                        )),
-                  );
-                });
-          },
-          child: LabelText("$selectedValue")),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: context.dynamicHeight(0.06),
+//       width: context.dynamicWidth(0.6),
+//       padding: context.paddingLow,
+//       decoration: BoxDecoration(
+//         border: Border.all(
+//           color: Colorsx.mainColor,
+//         ),
+//         borderRadius: BorderRadius.all(Radius.circular(25)),
+//       ),
+//       child: FlatButton(
+//           onPressed: () {
+//             showGeneralDialog(
+//                 context: context,
+//                 barrierDismissible: true,
+//                 barrierLabel:
+//                     MaterialLocalizations.of(context).modalBarrierDismissLabel,
+//                 barrierColor: Colors.black.withOpacity(0.8),
+//                 transitionDuration: const Duration(milliseconds: 300),
+//                 pageBuilder: (BuildContext buildContext, Animation animation,
+//                     Animation secondaryAnimation) {
+//                   return Center(
+//                     child: Container(
+//                         width: MediaQuery.of(context).size.width - 100,
+//                         height: MediaQuery.of(context).size.height - 100,
+//                         padding: EdgeInsets.all(20),
+//                         color: Colors.white,
+//                         child: RadioButtonWidget(
+//                           radioButtonName: this.radioButtonName,
+//                           selectedValue: selectedValue,
+//                         )),
+//                   );
+//                 });
+//           },
+//           child: LabelText("$selectedValue")),
+//     );
+//   }
+// }
--- lib/core/components/radiobutton.dart
+++ lib/core/components/radiobutton.dart
@@ -1,36 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:grouped_buttons/grouped_buttons.dart';
+// import 'package:flutter/material.dart';
+// import 'package:grouped_buttons/grouped_buttons.dart';
 
-class RadioButtonWidget extends StatelessWidget {
-  final List<String> radioButtonName;
-  final List<String> selectedValue;
+// class RadioButtonWidget extends StatelessWidget {
+//   final List<String> radioButtonName;
+//   final List<String> selectedValue;
 
-  const RadioButtonWidget({
-    Key key,
-    this.radioButtonName,
-    this.selectedValue,
-  }) : super(key: key);
+//   const RadioButtonWidget({
+//     Key key,
+//     this.radioButtonName,
+//     this.selectedValue,
+//   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: radioButtonName.length,
-        itemBuilder: (context, index) => Column(
-          children: [
-            RadioButtonGroup(
-                labels: <String>[
-                  '${radioButtonName[index]}',
-                ],
-                onSelected: (String selected) {
-                  Navigator.of(context).pop();
-                  print(selected);
-                  selectedValue.add(selected);
-                }),
-          ],
-          //
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: ListView.builder(
+//         itemCount: radioButtonName.length,
+//         itemBuilder: (context, index) => Column(
+//           children: [
+//             RadioButtonGroup(
+//                 labels: <String>[
+//                   '${radioButtonName[index]}',
+//                 ],
+//                 onSelected: (String selected) {
+//                   Navigator.of(context).pop();
+//                   print(selected);
+//                   selectedValue.add(selected);
+//                 }),
+//           ],
+//           //
+//         ),
+//       ),
+//     );
+//   }
+// }
